eee6fc9fc04fc068378b896b3a5f6eeddb57f3a0
Mahmoud AbdelGhani <m.moghni99@gmail.com>
2019-03-19 00:56:53 +0200

    Add Identity configuration in Database

--- Terkwaz.Api.Models/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Terkwaz.Api.Models
-{
-    public class Class1
-    {
-    }
-}
--- Terkwaz.Web.Api/Startup.cs
+++ Terkwaz.Web.Api/Startup.cs
@@ -1,14 +1,20 @@
 ﻿namespace Terkwaz.Web.Api
 {
     using Autofac;
+    using AutoMapper;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.IdentityModel.Tokens;
+    using Swashbuckle.AspNetCore.Swagger;
+    using System.Text;
     using Terkwaz.Data.Context;
     using Terkwaz.Web.Api.Bootstraper;
+    using Terkwaz.Web.Api.Identity;
 
     public class Startup
     {
@@ -29,11 +35,65 @@
 
             var contection = Configuration["ConnectionStrings:TerkwazDbConnection"];
             services.AddDbContext<TerkwazDbContext>(options => options.UseSqlServer(contection));
+
+            services.Configure<IdentityConfig>(Configuration.GetSection("IdentitySettings"));
+
+            var identitySettings = new IdentityConfig();
+            Configuration.GetSection("IdentitySettings")?.Bind(identitySettings);
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = identitySettings.Issuer,
+                        ValidAudience = identitySettings.Audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(identitySettings.SecurityKey))
+                    };
+                });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Version = "v1",
+                        Title = "Terkwaz APIs",
+                        Description = "Terkwaz - APIs documentation",
+                        TermsOfService = "none",
+                        Contact = new Contact
+                        {
+                            Name = "Mahmoud",
+                            Email = "m.moghni99@gmail.com"
+                        }
+                    });
+            });
+
+            Mapper.Initialize(cfg =>
+            {
+                cfg.AddProfile(new UserMapper());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                var contextService = serviceScope.ServiceProvider.GetService<TerkwazDbContext>();
+
+                if (contextService.AllMigrationsApplied())
+                {
+                    contextService.EnsureMigrated();
+                }
+            }
+
+            app.UseAuthentication();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -42,6 +102,13 @@
             {
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("../swagger/v1/swagger.json", "Terkwaz APIs v1");
+                c.RoutePrefix = "docs";
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
