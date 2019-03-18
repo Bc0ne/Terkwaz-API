70f2a9d5f7572d7d02c38990267b721ce009e885
Mahmoud AbdelGhani <m.moghni99@gmail.com>
2019-03-18 23:12:04 +0200

    Add User entity

---
+++ Terkwaz.Domain/User/User.cs
@@ -0,0 +1,33 @@
+﻿namespace Terkwaz.Domain.User
+{
+    public class User
+    {
+        public long Id { get; private set; }
+
+        public string FullName { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string PhotoUrl { get; private set; }
+
+        public byte[] PasswordHash { get; private set; }
+
+        public byte[] PasswordSalt { get; private set; }
+
+        public static User New(string fullName, string email, string photoUrl)
+        {
+            return new User
+            {
+                FullName = fullName,
+                Email = email,
+                PhotoUrl = photoUrl
+            };
+        }
+
+        public void UpdatePasswordHashAndSalt(byte[] passwordHash, byte[] passwordSalt)
+        {
+            PasswordHash = passwordHash;
+            PasswordSalt = passwordSalt;
+        }
+    }
+}
